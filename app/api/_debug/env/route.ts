ce023718b8d385ab23fe29ad81880ff4cb2f6040
MohammedMohsin404 <mohammedmohsin0725@gmail.com>
2025-09-06 04:20:09 +0600

    chek env

---
+++ app/api/_debug/env/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  return NextResponse.json({
+    NEXTAUTH_URL: !!process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: !!process.env.NEXTAUTH_SECRET,
+    AUTH_GOOGLE_ID: !!process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: !!process.env.AUTH_GOOGLE_SECRET,
+    MONGODB_URI: !!process.env.MONGODB_URI,
+  });
+}
